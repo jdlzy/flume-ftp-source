e8120f6637d68182a066780161f20565176b4275
lzy <kobeliuziyang@gmail.com>
2018-03-14 17:13:57 +0800

    commit

--- src/test/java/com/lzy/poi/test/ParseExcel.java
+++ src/test/java/com/lzy/poi/test/ParseExcel.java
@@ -4,8 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -17,6 +21,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.CellReference;
 
 import com.microsoft.schemas.office.visio.x2012.main.CellType;
+import org.keedio.flume.source.ftp.client.poi.XLSX2CSV2;
 
 /**
  * Created by taihe on 2018/3/12.
@@ -27,50 +32,44 @@ public class ParseExcel {
         InputStream inputStream = new FileInputStream("E:/newnsssssssss.xlsx");
         //InputStream inp = new FileInputStream("C:/Users/H__D/Desktop/workbook.xls");
 
-        Workbook workbook = WorkbookFactory.create(inputStream);
-        Sheet sheet = workbook.getSheetAt(0);
-
-        DataFormatter formatter = new DataFormatter();
-        for (Row row : sheet) {
-            for (Cell cell : row) {
-                CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
-                //单元格名称
-                System.out.print(cellRef.formatAsString());
-                System.out.print(" - ");
-
-                //通过获取单元格值并应用任何数据格式（Date，0.00，1.23e9，$ 1.23等），获取单元格中显示的文本
-                String text = formatter.formatCellValue(cell);
-                System.out.println(text);
-
-                //获取值并自己格式化
-                switch (cell.getCellType()) {
-                    case Cell.CELL_TYPE_STRING:// 字符串型
-                        System.out.println(cell.getRichStringCellValue().getString());
-                        break;
-                    case Cell.CELL_TYPE_NUMERIC:// 数值型
-                        if (DateUtil.isCellDateFormatted(cell)) { // 如果是date类型则 ，获取该cell的date值
-                            System.out.println(cell.getDateCellValue());
-                        } else {// 纯数字
-                            System.out.println(cell.getNumericCellValue());
-                        }
-                        break;
-                    case Cell.CELL_TYPE_BOOLEAN:// 布尔
-                        System.out.println(cell.getBooleanCellValue());
-                        break;
-                    case Cell.CELL_TYPE_FORMULA:// 公式型
-                        System.out.println(cell.getCellFormula());
-                        break;
-                    case Cell.CELL_TYPE_BLANK:// 空值
-                        System.out.println();
-                        break;
-                    case Cell.CELL_TYPE_ERROR: // 故障
-                        System.out.println();
-                        break;
-                    default:
-                        System.out.println();
-                }
-            }
-        }
-
+//        Workbook workbook = WorkbookFactory.create(inputStream);
+//        Workbook workbook=new XSSFWorkbook(inputStream);
+//
+//        Sheet sheet = workbook.getSheetAt(0);
+//
+//        DataFormatter formatter = new DataFormatter();
+//        for (Row row : sheet) {
+//            for (Cell cell : row) {
+//                CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
+//                //单元格名称
+//                System.out.print(cellRef.formatAsString());
+//                System.out.print(" - ");
+//
+//                //通过获取单元格值并应用任何数据格式（Date，0.00，1.23e9，$ 1.23等），获取单元格中显示的文本
+//                String text = formatter.formatCellValue(cell);
+//                System.out.println(text);
+//
+//            }
+//        }
+//        try {
+//
+//            List<String[]> list= XLSX2CSV2.getRecords("E:/newnsssssssss.xlsx",4);
+//            List<String> csvList=new ArrayList<String>();
+//for(String[] records:list){
+//            StringBuilder sb=new StringBuilder("");
+//
+//    for (String record:records){
+//        sb.append(record);
+//        sb.append(",");
+////        System.out.println(record);
+//    }
+//    csvList.add(sb.deleteCharAt(sb.length() - 1).toString());
+//}
+//for(String record:csvList){
+//    System.out.println(record);
+//}
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
--- src/test/java/com/lzy/poi/test/ParseExcelOntescomm.java
+++ src/test/java/com/lzy/poi/test/ParseExcelOntescomm.java
@@ -2,13 +2,20 @@ package com.lzy.poi.test;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.keedio.flume.source.ftp.client.poi.XLSX2CSV;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+
 import static org.apache.poi.xssf.usermodel.XSSFWorkbookType.XLSX;
 
 /**
@@ -16,26 +23,39 @@ import static org.apache.poi.xssf.usermodel.XSSFWorkbookType.XLSX;
  */
 public class ParseExcelOntescomm {
     public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
-
-        InputStream inputStream = new FileInputStream("E:/2g.xlsx");
+String path ="E:/2gTests.xlsx";
+//OutputStream os=new OutputStream("E:/csv/test");
+        InputStream inputStream = new FileInputStream(path);
         //InputStream inp = new FileInputStream("C:/Users/H__D/Desktop/workbook.xls");
 
-        Workbook workbook = new XSSFWorkbook(inputStream);
-        Sheet sheet = workbook.getSheetAt(0);
-
-        DataFormatter formatter = new DataFormatter();
-        for (Row row : sheet) {
-            if(row.getRowNum()==0){continue;}
-                StringBuilder sb=new StringBuilder();
-
-            for (Cell cell : row) {
-                sb.append(cell.getStringCellValue());
-                sb.append(",");
-            }
-            sb.deleteCharAt(sb.length()-1);
-            //输出每一行，使用逗号分隔
-            System.out.println(sb);
+//        Workbook workbook = new XSSFWorkbook(inputStream);
+//        Sheet sheet = workbook.getSheetAt(0);
+//
+//        DataFormatter formatter = new DataFormatter();
+//        for (Row row : sheet) {
+//            if(row.getRowNum()==0){continue;}
+//                StringBuilder sb=new StringBuilder();
+//
+//            for (Cell cell : row) {
+//                sb.append(cell.getStringCellValue());
+//                sb.append(",");
+//            }
+//        }
+//            sb.deleteCharAt(sb.length()-1);
+//            //输出每一行，使用逗号分隔
+//            System.out.println(sb);
+            OPCPackage p = OPCPackage.open(path, PackageAccess.READ);
+            XLSX2CSV xlsx2csv=new XLSX2CSV(p,System.out,-1);
+        try {
+            xlsx2csv.process();
+        } catch (OpenXML4JException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
         }
+
 
     }
 }
