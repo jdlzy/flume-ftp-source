d31c78d872bb1339f89c81b1538970baebebf23e
lzy <kobeliuziyang@gmail.com>
2018-03-19 12:19:16 +0800

    commit

--- src/test/java/com/lzy/poi/test/ParseExcel.java
+++ src/test/java/com/lzy/poi/test/ParseExcel.java
@@ -29,47 +29,46 @@ import org.keedio.flume.source.ftp.client.poi.XLSX2CSV2;
 public class ParseExcel {
     public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
 
-        InputStream inputStream = new FileInputStream("E:/newnsssssssss.xlsx");
-        //InputStream inp = new FileInputStream("C:/Users/H__D/Desktop/workbook.xls");
+       InputStream inputStream = new FileInputStream("E:/newsssssss.xlsx");
+     /*    //InputStream inp = new FileInputStream("C:/Users/H__D/Desktop/workbook.xls");
 
-//        Workbook workbook = WorkbookFactory.create(inputStream);
-//        Workbook workbook=new XSSFWorkbook(inputStream);
-//
-//        Sheet sheet = workbook.getSheetAt(0);
-//
-//        DataFormatter formatter = new DataFormatter();
-//        for (Row row : sheet) {
-//            for (Cell cell : row) {
-//                CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
-//                //单元格名称
-//                System.out.print(cellRef.formatAsString());
-//                System.out.print(" - ");
-//
-//                //通过获取单元格值并应用任何数据格式（Date，0.00，1.23e9，$ 1.23等），获取单元格中显示的文本
-//                String text = formatter.formatCellValue(cell);
-//                System.out.println(text);
-//
-//            }
-//        }
-//        try {
-//
-//            List<String[]> list= XLSX2CSV2.getRecords("E:/newnsssssssss.xlsx",4);
-//            List<String> csvList=new ArrayList<String>();
-//for(String[] records:list){
-//            StringBuilder sb=new StringBuilder("");
-//
-//    for (String record:records){
-//        sb.append(record);
-//        sb.append(",");
-////        System.out.println(record);
-//    }
-//    csvList.add(sb.deleteCharAt(sb.length() - 1).toString());
-//}
-//for(String record:csvList){
-//    System.out.println(record);
-//}
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        Workbook workbook = WorkbookFactory.create(inputStream);
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        DataFormatter formatter = new DataFormatter();
+        for (Row row : sheet) {
+
+                StringBuilder sb = new StringBuilder();
+
+                for (Cell cell : row) {
+                    sb.append(formatter.formatCellValue(cell));
+                    sb.append(",");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+System.out.println(sb.toString());
+            }*/
+        try {
+
+            List<String[]> list= XLSX2CSV2.getRecords(inputStream,3);
+            List<String> csvList=new ArrayList<String>();
+for(String[] records:list){
+            StringBuilder sb=new StringBuilder("");
+
+    for (String record:records){
+
+
+        sb.append(record);
+        sb.append(",");
+//        System.out.println(record);
+    }
+    csvList.add(sb.deleteCharAt(sb.length() - 1).toString());
+}
+for(String record:csvList){
+    System.out.println(record);
+}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
